dfdde3a69d75aa8c539b57aacd7802a12867714c
minh duc <minhductxqt@gmail.com>
2022-05-21 17:05:35 +0700

    add clientID to GoogleLogin

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,20 +1,33 @@
 import React from "react";
 import GoogleLogin from "react-google-login";
+// import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 import { useForm } from "react-hook-form";
 import logo from "../images/logo.png";
 
 const Signin = () => {
+  const onSuccess = (res) => {
+    console.log("[Login success] currentUser: ", res.profileObj);
+  };
+
+  const onFailure = (res) => {
+    console.log("[Login failed] res: ", res);
+  };
+
   const responseGoogle = (response) => {
     console.log(response);
     console.log(response.profileObj);
   };
 
-  const { register, handleSubmit, formState: {errors} } = useForm();
-  const onSubmit = data => console.log(data);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
 
   return (
     <div className="bg-sign-in bg-no-repeat bg-center bg-cover h-screen flex items-center justify-center">
-      <div className="bg-modal shadow-xl w-2/5 h-3/5 py-10 rounded-10 flex flex-col items-center justify-center">
+      <div className="bg-modal shadow-xl w-2/5 py-10 rounded-10 flex flex-col items-center justify-center">
         <div className="flex flex-col justify-center items-center">
           <img src={logo} alt="logo" />
           <span className="text-medium-blue font-extrabold text-3xl ml-2 font-logo">
@@ -24,21 +37,45 @@ const Signin = () => {
         <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
           <input
             type="text"
-            {...register("account", {required: true})}
+            {...register("account", { required: true })}
             placeholder="User Name or Email"
             className="outline-medium-blue border-solid border-gray border-1 mt-8 py-2 w-72 p-3 rounded-3 font-normal text-sm"
           />
-          {errors?.account?.type === "required" && <p className="text-xs mt-2 font-normal text-danger before:inline before:content-['⚠'] "> This field is required</p>}
+          {errors?.account?.type === "required" && (
+            <p className="text-xs mt-2 font-normal text-danger before:inline before:content-['⚠'] ">
+              {" "}
+              This field is required
+            </p>
+          )}
           <input
             type="password"
-            {...register("password", {required: true})}
+            {...register("password", {
+              required: true,
+              minLength: 6,
+              maxLength: 9,
+            })}
             placeholder="Password"
             className="outline-medium-blue border-solid border-gray border-1 mt-4 py-2 w-72 p-3 rounded-3 font-normal text-sm"
           />
-          {errors?.password?.type === "required" && <p className="text-xs my-2 font-normal text-danger before:inline before:content-['⚠'] "> This field is required</p>}
-          <button
-            className="bg-light-blue py-[0.6rem] mt-4 mb-2 font-semibold text-white rounded-3 hover:opacity-90 hover:text-gray"
-          >
+          {errors?.password?.type === "required" && (
+            <p className="text-xs my-2 font-normal text-danger before:inline before:content-['⚠'] ">
+              {" "}
+              This field is required
+            </p>
+          )}
+          {errors?.password?.type === "minLength" && (
+            <p className="text-xs my-2 font-normal text-danger before:inline before:content-['⚠'] ">
+              {" "}
+              Password must be 6 to 9 characters
+            </p>
+          )}
+          {errors?.password?.type === "maxLength" && (
+            <p className="text-xs my-2 font-normal text-danger before:inline before:content-['⚠'] ">
+              {" "}
+              Password must be 6 to 9 characters
+            </p>
+          )}
+          <button className="bg-light-blue py-[0.6rem] mt-4 mb-2 font-semibold text-white rounded-3 hover:opacity-90 hover:text-gray">
             Login
           </button>
           <span
@@ -49,7 +86,7 @@ const Signin = () => {
           </span>
           <div>
             <GoogleLogin
-              clientId="925214939780-qg7n3pqem43bv3qopl9jfjg4rthlg4p2.apps.googleusercontent.com"
+              clientId="518727150893-4c80ip0io9lbbnmbrujki5l8cn4vrvvv.apps.googleusercontent.com"
               buttonText="Login via Google"
               onSuccess={responseGoogle}
               onFailure={responseGoogle}
