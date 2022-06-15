bcc6dc8269701a1be4b8b1ad03df8318f7e850c3
minh duc <minhductxqt@gmail.com>
2022-06-15 11:14:07 +0700

    fix home UI

--- src/components/TripSearch/TripSearch.js
+++ src/components/TripSearch/TripSearch.js
@@ -22,15 +22,15 @@ const TripSearch = () => {
   return (
     <div>
       <div className="flex flex-col">
-        <h1 className="ml-4 mt-10 text-3xl font-extrabold">
+        <h1 className="ml-4 mt-14 text-4xl font-extrabold mb-10">
           Go with TriPari's
         </h1>
         <div className="pt-8 flex px-3 flex-between bg-[#f2f4f5]">
           <div className="w-1/3 flex flex-col items-center justify-left pr-4">
-            <h1 className="flex  text-left text-5xl w-3/4 mb-20 mt-20 font-black">
+            <h1 className="flex  text-left text-4xl w-3/4 mb-14 mt-20 font-black">
               Take a trip with TripPari’s
             </h1>
-            <p className="flex  text-left text-2xl w-3/4 font-medium">
+            <p className="flex  text-left text-xl w-3/4 font-medium">
               TriPari’s is a platform that connects you with an experienced
               traveler or with an expat living in that corner of the world you
               are interested in. You can buy the trip as a package with a
@@ -42,30 +42,26 @@ const TripSearch = () => {
             <div className="w-[450px] relative mb-8  hover:scale-[1.02] hover:duration-[0.1s] hover:ease-in">
               <Link to="/home">
                 <img src={des5} alt="" className="opacity-100" />
-                
               </Link>
             </div>
             <div className="w-[450px] relative ml-16 mb-8 hover:scale-[1.02] hover:duration-[0.1s] hover:ease-in">
               <Link to="/home">
                 <img src={des5} alt="" className="opacity-100" />
-                
               </Link>
             </div>
             <div className="w-[450px] relative mb-8 hover:scale-[1.02] hover:duration-[0.1s] hover:ease-in">
               <Link to="/home">
                 <img src={des5} alt="" className="opacity-100" />
-                
               </Link>
             </div>
             <div className="w-[450px] relative ml-16 mb-8 hover:scale-[1.02] hover:duration-[0.1s] hover:ease-in">
               <Link to="/home">
                 <img src={des5} alt="" className="opacity-100" />
-                
               </Link>
             </div>
           </div>
         </div>
-        <h1 className="ml-4 mt-20 text-3xl font-extrabold">
+        <h1 className="ml-4 mt-20 mb-12 text-3xl font-extrabold">
           Can’t decide where? Get inspired
         </h1>
         <div className="pt-8 flex px-3 flex-between bg-[#f2f4f5]">
